8a07d1183900f3c63a25f1c2e00009bd4af34b8f
alionaterguta <71376066+alionaterguta@users.noreply.github.com>
2024-04-08 09:02:38 +0300

    Firestore DB anonymous auth  and messages collection created

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
-import { StatusBar } from "expo-status-bar";
 import Start from "./components/start";
 import Chat from "./components/chat";
 
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { LogBox } from "react-native";
+LogBox.ignoreLogs([
+  "[2024-04-07T20:44:48.130Z]  @firebase/auth: Auth (10.3.1)",
+]);
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
+  // The web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBbGBqyhFhukNqlRwDvuzK0D7ySSIp8T-Q",
+    authDomain: "chatapp-aaafb.firebaseapp.com",
+    projectId: "chatapp-aaafb",
+    storageBucket: "chatapp-aaafb.appspot.com",
+    messagingSenderId: "1050968563314",
+    appId: "1:1050968563314:web:10b701bfbf8cf8a9e16ceb",
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const db = getFirestore(app);
+
   return (
     /* Wrap the app with NavigationContainer */
     <NavigationContainer>
       {/* Create a stack navigator with initial route Start  */}
       <Stack.Navigator initialRouteName="Start">
         <Stack.Screen name="Start" component={Start} />
-        <Stack.Screen name="Chat" component={Chat} />
+        <Stack.Screen name="Chat">
+          {(props) => <Chat {...props} db={db} />}
+        </Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
   );
